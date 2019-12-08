9f97ebfbb10d98b63da434adf38525a97488546c
madyanalj <22075444+madyanalj@users.noreply.github.com>
2019-12-08 20:14:49 +0000

    refactor: inject content script dynamically to avoid requiring all_urls permission

--- src/popup/index.tsx
+++ src/popup/index.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { isInDevMode } from '../shared/environemnt';
 import { App } from './App';
 
 if (module.hot) {
   module.hot.accept();
+}
+
+if (!isInDevMode) {
+  // inject content script
+  chrome.tabs.executeScript({ file: 'content/index.js' });
 }
 
 ReactDOM.render(<App />, document.getElementById('root'));
